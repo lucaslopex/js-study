8ad5afd64254b4c012bcdc8404731c1cf8bd274d
Lucas Silva <lucaslopessilva17@gmail.com>
2023-07-21 21:44:29 -0300

    condições (escolar)

--- Lógica/Condições/index.js
+++ Lógica/Condições/index.js
@@ -1,4 +1,4 @@
-const hora = 40;
+const hora = 12;
 
 if (hora >= 0 && hora <= 11)  {
     console.log('Bom dia!');
@@ -11,5 +11,25 @@ else if (hora >= 18 && hora <= 23) {
     console.log('Boa noite');
 }
 else{
-    console.log('Horário invalido')
+    console.log('Horário invalido');
 }
+
+const nota = 6;
+
+if (nota < 6) {
+    console.log('Reprovado')
+}
+else if (nota >= 6 && nota <= 7) {
+    console.log('Aprovado em média');
+}
+else if (nota >= 7 && nota <= 9) {
+    console.log('Aprovado com nota acima da média');
+}
+else if (nota == 10) {
+    console.log('Aprovado com êxito');
+}
+else {
+    console.log('Nota inválida');
+}
+console.log(`Nota do aluno(a) é: ${nota}`)
+console.log('Nota escolar exibida com sucesso')
