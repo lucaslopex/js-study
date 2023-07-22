ed27841dae77abe66c3f2bfa931134e81dca0d02
Lucas Silva <lucaslopessilva17@gmail.com>
2023-07-21 21:02:17 -0300

    condições

---
+++ Lógica/Condições/index.js
@@ -0,0 +1,15 @@
+const hora = 40;
+
+if (hora >= 0 && hora <= 11)  {
+    console.log('Bom dia!');
+}
+else if (hora >= 12 && hora <= 17) {
+    console.log('Boa tarde');
+    
+}
+else if (hora >= 18 && hora <= 23) {
+    console.log('Boa noite');
+}
+else{
+    console.log('Horário invalido')
+}
