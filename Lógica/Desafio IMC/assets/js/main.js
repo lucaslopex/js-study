b1aed4f180a0ccea54954890bf5e15d9e15b862b
Lucas Silva <lucaslopessilva17@gmail.com>
2023-08-05 15:36:26 -0300

    IMC resolvido

--- Lógica/Desafio IMC/assets/js/main.js
+++ Lógica/Desafio IMC/assets/js/main.js
@@ -1,50 +1,50 @@
 const form = document.querySelector('.form');
 form.addEventListener('submit', imc);
-
 function imc(evento) {
-
+    
         evento.preventDefault();
         const ipeso = form.querySelector('#peso');
         const ialtura = form.querySelector('#altura');
         
         const peso = Number(ipeso.value);
-        const altura = Number(ialtura.value*100);
+        const altura = Number(ialtura.value);
 
         console.log(peso);
         console.log(altura);
 
-        var calculo = peso/altura
+        var calculo = peso/(altura * altura)
+        calculo = calculo.toFixed(2)
+        console.log(calculo);
         var resultado
 
         
-        if (calculo <= 18, 5) {
+        if (calculo <= 18.5) {
             
-            return `Seu IMC é de: ${calculo}. (Abaixo do peso)`;
+            resultado = `Seu IMC é de: ${calculo} (Abaixo do peso)`;
         }
-        else if (calculo > 18,5 && calculo <= 24,9) {
-            return `Seu IMC é de: ${calculo}. (Peso dentro da normalidade)`;
+        else if (calculo > 18.5 && calculo <= 24.9) {
+            resultado = `Seu IMC é de: ${calculo} (Peso dentro da normalidade)`;
         }
-        else if (calculo => 25 && calculo <= 29,9) {
-            return `Seu IMC é de: ${calculo}. (Sobrepeso)`;
+        else if (calculo >= 25 && calculo <= 29.9) {
+            resultado = `Seu IMC é de: ${calculo} (Sobrepeso)`;
         }
-        else if (calculo => 30 && calculo <= 34,9) {
-            return `Seu IMC é de: ${calculo}. (Obesidade nível 1)`;
+        else if (calculo >= 30 && calculo <= 34.9) {
+            resultado = `Seu IMC é de: ${calculo} (Obesidade nível 1)`;
         }
-        else if (calculo => 35 && calculo <= 39,9) {
-            return `Seu IMC é de: ${calculo}. (Obesidade nível 2)`;
+        else if (calculo >= 35 && calculo <= 39.9) {
+            resultado = `Seu IMC é de: ${calculo} (Obesidade nível 2)`;
         }
-        else if (calculo => 40) {
-            return `Seu IMC é de: ${calculo}. (Obesidade nível 3)`;
+        else if (calculo >= 40) {
+            resultado = `Seu IMC é de: ${calculo} (Obesidade nível 3)`;
         }
         else
         {
-            return `Dados inválidos`;
+            resultado = `Dados inválidos`;
         }
 
+        console.log (resultado);
         //Inves de retornar o valor e o resultado, fazer outra função com os resultados e imprimir no html
 
-        function imprimiResultado(imc) {
-            
-            
-        }
+        const resultadoIMC = document.getElementById('resultado-imc');
+        resultadoIMC.innerHTML = resultado;
     };
